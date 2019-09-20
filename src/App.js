b8e350459918f7b2160967ccd3a529413f3d9ae8
Steven Ha <steven_ha666@hotmail.com>
2019-09-19 22:57:35 -0400

    created table display for to-dos and add table css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,48 @@ import Navbar from './components/Navbar';
 import Todos from './components/Todos';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tempTodos: [
+        {
+          id: 1,
+          description: "Get out of bed",
+          completed: false,
+          priority: "medium"
+        },
+        {
+          id: 2,
+          description: "Brush Teeth",
+          completed: false,
+          priority: "high"
+        },
+        {
+          id: 3,
+          description: "Each breakfast",
+          completed: false,
+          priority: "low"
+        },
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar> </Navbar>
-        <Todos></Todos>
+        <Navbar/>
+        <table>
+          <tr>
+            <th>Description</th>
+            <th>Priority</th>
+            <th>Action</th>
+            <th>Mark Done</th>
+            <th>Delete Todo</th>
+          </tr>
+          {this.state.tempTodos.map(todo => {
+          return <Todos todo={todo}/>
+        })}
+        </table>
       </div>
     );
   }
--- src/components/Todos/todos.js
+++ src/components/Todos/todos.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import './todos.scss'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 class Todos extends Component {
   render() {
+    const { id, description, completed, priority } = this.props.todo;
     return (
-      <div>
-        <h1> Hello from Todos</h1> 
-      </div>
+      <tr className="todo-task">
+        <td> { description } </td>
+        <td> { priority.toUpperCase() } </td>
+        <td> Edit </td>
+        <td><FontAwesomeIcon icon={faCheckCircle} className="logo-icon"/></td>
+        <td><FontAwesomeIcon icon={faTimesCircle} className="logo-icon"/></td>
+      </tr>
     );
   }
 }
